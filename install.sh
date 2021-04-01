5f6365e6f2bf2638419e642b273a0df968d685e9
Xinye Li <lixinye@gmail.com>
2021-04-01 16:03:26 +0100

    Add a few symlinks

--- install.sh
+++ install.sh
@@ -4,7 +4,13 @@
 ln -fs $HOME/dotfiles/zsh/.zshrc $HOME/.zshrc
 ln -fs $HOME/dotfiles/vim/.vimrc $HOME/.vimrc
 
+ln -fs $HOME/dotfiles/lsd/.config/lsd/config.yaml $HOME/.config/lsd/config.yaml
 ln -fs $HOME/dotfiles/dunst/.config/dunst/dunstrc $HOME/.config/dunst/dunstrc
 ln -fs $HOME/dotfiles/i3/.i3/config $HOME/.i3/config
 ln -fs $HOME/dotfiles/i3/.i3/i3blocks.config $HOME/.i3/i3blocks.config
 ln -fs $HOME/dotfiles/kitty/kitty.config $HOME/.config/kitty/kitty.config
+
+ln -fs $HOME/dotfiles/rofi/.config/rofi/config.rasi $HOME/.config/rofi/config.rasi
+ln -fs $HOME/dotfiles/rofi/.config/rofi/xl-common.rasi $HOME/.config/rofi/xl-common.rasi
+ln -fs $HOME/dotfiles/rofi/.config/rofi/xl-manjaro.rasi $HOME/.config/rofi/xl-manjaro.rasi
+
