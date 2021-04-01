79f37406ddc5dd5ba8c47c2674ca9f321c89db98
Xinye Li <lixinye@gmail.com>
2021-04-01 18:55:10 +0100

    Add kitty-theme download to install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,20 @@
 #! /usr/bin/bash
 
-# Symlinking
+# HOME
 ln -fs $HOME/dotfiles/zsh/.zshrc $HOME/.zshrc
 ln -fs $HOME/dotfiles/vim/.vimrc $HOME/.vimrc
 
-ln -fs $HOME/dotfiles/lsd/.config/lsd/config.yaml $HOME/.config/lsd/config.yaml
-ln -fs $HOME/dotfiles/dunst/.config/dunst/dunstrc $HOME/.config/dunst/dunstrc
+# i3
 ln -fs $HOME/dotfiles/i3/.i3/config $HOME/.i3/config
 ln -fs $HOME/dotfiles/i3/.i3/i3blocks.config $HOME/.i3/i3blocks.config
-ln -fs $HOME/dotfiles/kitty/kitty.config $HOME/.config/kitty/kitty.config
 
+# rofi
 ln -fs $HOME/dotfiles/rofi/.config/rofi/config.rasi $HOME/.config/rofi/config.rasi
 ln -fs $HOME/dotfiles/rofi/.config/rofi/xl-common.rasi $HOME/.config/rofi/xl-common.rasi
 ln -fs $HOME/dotfiles/rofi/.config/rofi/xl-manjaro.rasi $HOME/.config/rofi/xl-manjaro.rasi
 
+# utility
+ln -fs $HOME/dotfiles/picom/.config/picom/picom.conf $HOME/.config/kitty/kitty.conf
+ln -fs $HOME/dotfiles/kitty/.config/kitty/kitty.conf $HOME/.config/kitty/kitty.conf
+ln -fs $HOME/dotfiles/lsd/.config/lsd/config.yaml $HOME/.config/lsd/config.yaml
+ln -fs $HOME/dotfiles/dunst/.config/dunst/dunstrc $HOME/.config/dunst/dunstrc
