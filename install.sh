00ca7ed25446bb2c3c60a27d2858b3ba06613e7d
Xinye Li <lixinye@gmail.com>
2021-04-01 15:07:44 +0100

    Add first version of install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/bash
+
+# Symlinking
+ln -fs $HOME/dotfiles/zsh/.zshrc $HOME/.zshrc
+ln -fs $HOME/dotfiles/vim/.vimrc $HOME/.vimrc
+
+ln -fs $HOME/dotfiles/dunst/.config/dunst/dunstrc $HOME/.config/dunst/dunstrc
+ln -fs $HOME/dotfiles/i3/.i3/config $HOME/.i3/config
+ln -fs $HOME/dotfiles/i3/.i3/i3blocks.config $HOME/.i3/i3blocks.config
+ln -fs $HOME/dotfiles/kitty/kitty.config $HOME/.config/kitty/kitty.config
