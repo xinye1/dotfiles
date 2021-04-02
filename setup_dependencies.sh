0d26cb7fc9220946c2f8f61d7f953c7cc2c812d8
Xinye Li <lixinye@gmail.com>
2021-04-02 15:32:50 +0100

    Clean up setup script, install p10k and change /usr/bin/vim  manually

--- setup_dependencies.sh
+++ setup_dependencies.sh
@@ -4,7 +4,7 @@
 echo "Turning off the terminal beeping..."
 echo "blacklist pcspkr" | sudo tee /etc/modprobe.d/nobeep.conf
 
-# a few utils
+# install stuff
 echo "Installing utils and fonts..."
 sudo pacman -S --noconfirm \
   xdotool \
@@ -18,49 +18,32 @@ sudo pacman -S --noconfirm \
   unzip \
   ttf-roboto \
   ttf-font-awesome \
-  ttf-cascadia-code
+  ttf-cascadia-code \
+  gcc-fortran # R dependency
 
-# utilities to make R work
-echo "Installing R package dependencies"
-sudo pacman -S --noconfirm \
-  gcc-fortran
-
-# install kitty
-echo "Installing kitty..."
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-git clone --depth 1 git@github.com:dexpota/kitty-themes.git \
-  $HOME/.config/kitty/kitty-themes
-
-# make zsh the default shell
-echo "zsh for default shell..."
+echo "Make zsh the default shell..."
 chsh -s $(which zsh)
 
-# grab oh-my-zsh and plugins
 echo "Setting up oh-my-zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-# grap i3blocks-contrib
+echo "Installing kitty..."
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+git clone --depth 1 git@github.com:dexpota/kitty-themes.git $HOME/.config/kitty/kitty-themes
+
 echo "Grabbing i3blocks-contrib"
-git clone \
-  git@github.com:vivien/i3blocks-contrib.git \
-  $HOME/.i3/blocklets
+git clone git@github.com:vivien/i3blocks-contrib.git $HOME/.i3/blocklets
 
-# zsh plugins
-echo "Grabbing zsh plugins"
-git clone \
-  https://github.com/zsh-users/zsh-autosuggestions.git \
-  $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone \
-  https://github.com/zsh-users/zsh-syntax-highlighting.git \
-  $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+echo "Grabbing zsh plugins..."
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
-# vim packages
-git clone \
-  https://github.com/itchyny/lightline.vim \
-  $HOME/.vim/pack/plugins/start/lightline
-git clone \
-  https://tpope.io/vim/fugitive.git \
-  $HOME/.vim/pack/plugins/start/fugitive
+echo "Vim packages..."
+git clone https://github.com/itchyny/lightline.vim $HOME/.vim/pack/plugins/start/lightline
+git clone https://tpope.io/vim/fugitive.git $HOME/.vim/pack/plugins/start/fugitive
+
+# manual stuff
+echo "Restart system and run `git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k`"
+echo "Change $EDITOR var to /usr/bin/vim in ~/.profile"
