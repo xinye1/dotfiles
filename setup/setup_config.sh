f54b22510cab3c853f8d01943858700d498d0778
Xinye Li <lixinye@gmail.com>
2021-04-03 13:23:37 +0100

    Fix setup_config.sh typo

--- setup/setup_config.sh
+++ setup/setup_config.sh
@@ -3,5 +3,5 @@ sed -i 's+export EDITOR=/usr/bin/nano+export EDITOR=/usr/bin/vim+g' ~/.profile
 sed -i 's+export BROWSER=/usr/bin/palemoon+#export BROWSER=/usr/bin/palemmon+g' ~/.profile
 
 # ===== profile ======
-sed -i 's/set show_hidden false/setshow_hidden true/g' ~/.config/ranger/rc.conf
+sed -i 's/set show_hidden false/set show_hidden true/g' ~/.config/ranger/rc.conf
 
