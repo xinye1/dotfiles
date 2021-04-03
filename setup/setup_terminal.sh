c293870e8e497c52c5ee61a5fc8be3cef969972d
Xinye Li <lixinye@gmail.com>
2021-04-03 12:37:38 +0100

    Resolve missing /home/xinye/.oh-my-zsh/custom

--- setup/setup_terminal.sh
+++ setup/setup_terminal.sh
@@ -10,9 +10,10 @@ git clone --depth 1 https://github.com/dexpota/kitty-themes.git $HOME/.config/ki
 echo "===== Setting up oh-my-zsh ====="
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo "===== Setting up oh-my-zsh ====="
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-
 echo "===== Power Level 10k ====="
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+echo "===== oh-my-zsh plugins ====="
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
