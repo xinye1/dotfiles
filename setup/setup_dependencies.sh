ade3b1fb5aa27dec3ca93f0aa343d39d7e17671b
Xinye Li <lixinye@gmail.com>
2021-04-04 19:49:16 +0100

    Make i3blocks and add vim plugin colorizer

--- setup/setup_dependencies.sh
+++ setup/setup_dependencies.sh
@@ -21,6 +21,10 @@ sudo pacman -S --noconfirm \
   ttf-cascadia-code \
   gcc-fortran # R dependency
 
+echo "===== Make i3blocks ====="
+make -C $HOME/.i3/blocklets/bandwidth2/
+make -C $HOME/.i3/blocklets/cpu_usage2/
+
 echo "===== Grabbing i3blocks-contrib ====="
 git clone https://github.com/vivien/i3blocks-contrib.git $HOME/.i3/blocklets
 
@@ -30,3 +34,4 @@ cp $HOME/dotfiles/i3/rofi-calendar $HOME/.i3/blocklets/rofi-calendar/
 echo "===== Vim packages ======"
 git clone https://github.com/itchyny/lightline.vim $HOME/.vim/pack/plugins/start/lightline
 git clone https://tpope.io/vim/fugitive.git $HOME/.vim/pack/plugins/start/fugitive
+git clone https://github.com/lilydjwg/colorizer.git $HOME/.vim/pack/plugins/start/colorizer
