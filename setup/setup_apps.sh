83c300cb8890da5c299c11fb404684a1d3c63d1d
Xinye Li <lixinye@gmail.com>
2021-04-04 19:52:22 +0100

    Build chrome, vscode and rstudio, install r

---
+++ setup/setup_apps.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/bash
+
+pamac build --no-confirm \
+  google-chrome \
+  visual-studio-code-bin \
+  rstudio-desktop-bin
+
+sudo pacman -S --noconfirm r
+
+# use Intel MKL for R
+#if
+#  [ $(inxi -C | grep -i intel | wc -l) -eq 1 ]
+#then
+#  pamac install --no-confirm intel-mkl
+#  pamac build --no-confirm r-mkl
+#else
+#  pacman -S --no-confirm r
+#fi
+
