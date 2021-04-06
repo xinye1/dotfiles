1317c9d2c405c71e8110007fcfd68df23c0ca5b2
Xinye Li <lixinye@gmail.com>
2021-04-06 19:35:06 +0100

    Remove picom link but edit existing conf

--- setup/setup_config.sh
+++ setup/setup_config.sh
@@ -1,13 +1,12 @@
 # ===== sync time ======
 timedatectl set-ntp true
 
-# ===== ranger =====
-#sed -i 's+export EDITOR=/usr/bin/nano+export EDITOR=/usr/bin/vim+g' ~/.profile
-#sed -i 's+export BROWSER=/usr/bin/palemoon+#export BROWSER=/usr/bin/palemmon+g' ~/.profile
-
 # ===== profile ======
 ranger --copy-config=all
 sed -i 's/set show_hidden false/set show_hidden true/g' ~/.config/ranger/rc.conf
+
+# ===== picom ======
+sed -i 's/fading = false;/fading = true;/g' ~/.config/picom.conf
 
 # ===== nitrogen ======
 #sed -i 's/bgcolor=#002a36/bgcolor=#3B4252/g' ~/.config/nitrogen/bg-saved.cfg
--- setup/symlink.sh
+++ setup/symlink.sh
@@ -12,7 +12,7 @@ ln -fs $HOME/dotfiles/vim/.vimrc $HOME/.vimrc
 ln -fs $HOME/dotfiles/zsh/.oh-my-zsh $HOME/.oh-my-zsh/oh-my-zsh.sh
 
 # i3
-#ln -fs $HOME/dotfiles/i3/.i3/config $I3_HOME/config
+ln -fs $HOME/dotfiles/i3/.i3/config $I3_HOME/config
 ln -fs $HOME/dotfiles/i3/.i3/i3blocks.config $I3_HOME/i3blocks.config
 mkdir -p $I3_HOME/scripts
 ln -fs $HOME/dotfiles/scripts/matrixlock.sh $I3_HOME/scripts/matrixlock.sh
@@ -32,14 +32,9 @@ ln -fs $HOME/dotfiles/lsd/.config/lsd/config.yaml $HOME/.config/lsd/config.yaml
 mkdir -p $HOME/.config/termite
 ln -fs $HOME/dotfiles/termite/.config/termite/config $HOME/.config/termite/config
 
-# kitty
-#mkdir -p $HOME/.config/kitty
-#ln -fs $HOME/dotfiles/kitty/.config/kitty/kitty.conf $HOME/.config/kitty/kitty.conf
-#ln -fs $HOME/dotfiles/kitty/.config/kitty/nord.conf $HOME/.config/kitty/nord.conf
-
 # picom
 #mkdir -p $HOME/.config/picom
-#ln -fs $HOME/dotfiles/picom/.config/picom/picom.conf $HOME/.config/picom/picom.conf
+#ln -fs $HOME/dotfiles/picom/.config/picom/picom.conf $HOME/.config/picom.conf
 
 # dunst
 ln -fs $HOME/dotfiles/dunst/.config/dunst/dunstrc $HOME/.config/dunst/dunstrc
